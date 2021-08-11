0bd32096ffb5b70384c54819f2c9e4ea041e57de
Istine Baldes <istinebaldes@gmail.com>
2021-08-12 03:46:39 +0800

    Add test for login

--- PageObjects/LoginPage.cs
+++ PageObjects/LoginPage.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace SeleniumCsharpPractice.PageObjects
 {
     class LoginPage
     {
+        public IWebDriver driver;
+        private WebDriverWait wait;
+        Int32 timeout = 10000; // in milliseconds
+
+        //Locators
+        private By userNameTextfield = By.CssSelector("#user-name");
+        private By passwordTextfield = By.CssSelector("#password");
+        private By loginBtn = By.CssSelector("#login-button");
+        private By loginErrorMsg = By.CssSelector("[data-test='error']");
+        private By loginErrorMsgX = By.CssSelector(".error-button");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public void EnterUsername(string username)
+        {
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(userNameTextfield));
+            driver.FindElement(userNameTextfield).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(passwordTextfield));
+            driver.FindElement(passwordTextfield).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(loginBtn).Click();
+        }
+
+        public void EnterCredentialsAndLogin(string username, string password)
+        {
+            EnterUsername(username);
+            EnterPassword(password);
+            ClickLoginButton();
+        }
+
+        public string LoginErrorMessage()
+        {
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(loginErrorMsg));
+            return driver.FindElement(loginErrorMsg).Text;
+        }
+
+        public void CloseLoginErrorMessage()
+        {
+            driver.FindElement(loginErrorMsgX).Click();
+        }
     }
 }
---
+++ Tests/SuccessLoginTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumCsharpPractice.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCsharpPractice.Tests
+{
+    class SuccessLoginTest
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void NavigateToLoginSite()
+        {
+            // Local Selenium WebDriver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [Test, Description("Successful Login")]
+        [TestCase("standard_user")]
+        [TestCase("problem_user")]
+        [TestCase("performance_glitch_user")]
+        public void ShouldBeAbleToLoginSuccessfully(string username)
+        {
+            var login = new LoginPage(driver);
+            login.EnterCredentialsAndLogin(username, "secret_sauce");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            var homepage = new HomePage(driver);
+            Assert.IsTrue(homepage.IsNavigatedToHomePage());
+        }
+
+        [TearDown]
+        public void Logout()
+        {
+            var homepage = new HomePage(driver);
+            homepage.LogoutFromSite();
+        }
+
+        [OneTimeTearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
