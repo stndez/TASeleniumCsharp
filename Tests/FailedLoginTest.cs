991d800f267f3c3cc12bb0919f52380550d89701
Istine Baldes <istinebaldes@gmail.com>
2021-08-12 00:05:14 +0800

    draft

---
+++ Tests/FailedLoginTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCsharpPractice.Tests
+{
+    class FailedLoginTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void BrowserInitialization()
+        {
+            // Local Selenium WebDriver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Sample()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
